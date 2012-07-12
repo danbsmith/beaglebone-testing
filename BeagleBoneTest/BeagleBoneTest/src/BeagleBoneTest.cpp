51aca81d7e7172a87853f1d714dc8da217224e7c
Daniel Smith <daniel@smitbo.com>
2012-07-12 15:29:17 -0400

    Changed to test PWM functions.

--- BeagleBoneTest/BeagleBoneTest/src/BeagleBoneTest.cpp
+++ BeagleBoneTest/BeagleBoneTest/src/BeagleBoneTest.cpp
@@ -1,9 +1,9 @@
 //============================================================================
 // Name        : BeagleBoneTest.cpp
-// Author      : 
+// Author      : Daniel Smith
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Test Program for C++ on BeagleBone
 //============================================================================
 
 #include <iostream>
@@ -14,39 +14,18 @@
 using namespace std;
 
 int main() {
-	cout << "Pin exported!" << endl;
-	exportPin(48);
-	exportPin(7);
-	exportPin(38);
 	clock_t time = clock();
 	clock_t temp_t;
 	temp_t = clock();
-	int flag = 0;
-	digitalWrite(48, 1);
-	digitalWrite(7, 1);
+	enablePWMMUX(10);
+	setPWM(1,0,50,100);
 	while (time < (15 * CLOCKS_PER_SEC)) {
 		temp_t = clock();
 		//float delay = (float) analogRead(1);
 		//cout << delay << endl;
-		if ((temp_t - time) >= (CLOCKS_PER_SEC / 2000)) {
-			if (flag == 0) {
-				cout << "Set to high!" << endl;
-				digitalWrite(38, 1);
-				flag = 1;
-			} else {
-				cout << "Set to low!" << endl;
-				digitalWrite(38, 0);
-				flag = 0;
-			}
 			time = clock();
-		}
 		temp_t = clock();
 	}
-	digitalWrite(48, 0);
-	digitalWrite(7, 0);
-	digitalWrite(38, 0);
-	unExportPin(48);
-	unExportPin(7);
-	unExportPin(38);
+	setPWM(1,0,0,100);
 	return 0;
 }
